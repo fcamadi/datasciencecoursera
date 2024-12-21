3390a6c3c97fa1da0467ce57be0be1b8e1c3d0e9
Fran Camacho <fcamadi@gmail.com>
2024-12-21 19:43:23 +0100

    minor changes to week 2 assignment

--- Week2_ProgAssignment.R
+++ Week2_ProgAssignment.R
@@ -52,7 +52,7 @@ complete("specdata", 3)
 
 
 # Week 2 Prog. Assignment - Part 3.
-corrrrr <- function(directory, threshold=0) {
+cor <- function(directory, threshold=0) {
   
   files <- list.files(directory)
   data <- vector(mode = "numeric")
@@ -69,7 +69,7 @@ corrrrr <- function(directory, threshold=0) {
   data
 }
 
-cr <- corrrrr("specdata", 150)
+cr <- cor("specdata", 150)
 head(cr)
 summary(cr)
 
@@ -81,3 +81,43 @@ summary(cr)
 cr <- corr("specdata")
 summary(cr)
 length(cr)
+
+#Quiz questions:
+
+pollutantmean("specdata", "sulfate", 1:10)
+pollutantmean("specdata", "nitrate", 70:72)
+pollutantmean("specdata", "sulfate", 34)
+pollutantmean("specdata", "nitrate")
+
+
+cc <- complete("specdata", c(6, 10, 20, 34, 100, 200, 310))
+print(cc$nobs)
+cc <- complete("specdata", 54)
+print(cc$nobs)
+
+RNGversion("3.5.1")  
+set.seed(42)
+cc <- complete("specdata", 332:1)
+use <- sample(332, 10)
+print(cc[use, "nobs"])
+
+cr <- corr("specdata")                
+cr <- sort(cr)   
+RNGversion("3.5.1")
+set.seed(868)                
+out <- round(cr[sample(length(cr), 5)], 4)
+print(out)
+
+cr <- corr("specdata", 129)                
+cr <- sort(cr)                
+n <- length(cr)    
+RNGversion("3.5.1")
+set.seed(197)                
+out <- c(n, round(cr[sample(n, 5)], 4))
+print(out)
+
+cr <- corr("specdata", 2000)                
+n <- length(cr)                
+cr <- corr("specdata", 1000)                
+cr <- sort(cr)
+print(c(n, round(cr, 4)))
